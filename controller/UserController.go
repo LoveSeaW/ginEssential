a492ff690046493df515c82450129ab5d1a46e27
caowei <1840065113@qq.com>
2023-09-25 12:38:01 +0800

    封装统一格式

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"ginEssential/common"
+	"ginEssential/dto"
 	"ginEssential/model"
+	"ginEssential/response"
 	"ginEssential/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -19,11 +21,11 @@ func Register(c *gin.Context) {
 	password := c.PostForm("password")
 	//数据验证
 	if len(telephone) != 11 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
 		return
 	}
 	if len(password) < 6 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "密码不能少于6位")
 		return
 	}
 	//如果名称没有传，就给一个10位的字符串
@@ -35,7 +37,7 @@ func Register(c *gin.Context) {
 
 	//判断手机号是否存在
 	if isTelephoneExist(common.DB, telephone) {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户存在不允许注册"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "用户存在不允许注册")
 		return
 	}
 
@@ -102,7 +104,7 @@ func Login(c *gin.Context) {
 func Info(c *gin.Context) {
 	user, _ := c.Get("user")
 
-	c.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": user}})
+	c.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": dto.ToUserDto(user.(model.User))}})
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
